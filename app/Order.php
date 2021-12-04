9a302a392ab2ae7cc76dce3f6623b2a092e41e44
Angelo7x <abennici96@gmail.com>
2021-12-04 12:07:23 +0100

    add pivot table food_order

--- app/Order.php
+++ app/Order.php
@@ -8,6 +8,7 @@ class Order extends Model
 {
 
     protected $fillable = ['user_id', 'ui_name', 'ui_delivery_address', 'ui_email', 'ui_phone', 'ui_delivery_info', 'total_price', 'status' ];
+    
     public function user() {
         return $this->belongsTo("App\User");
     }
