1cf088d0249660988819298ab299deb1ad2a2eea
DavideBernardini <davidebernardini09@gmail.com>
2021-12-08 12:00:58 +0100

    add header structure

--- resources/js/guest/App.vue
+++ resources/js/guest/App.vue
@@ -14,6 +14,24 @@ export default {
 	name: 'App',
 	components: {
         Header,
-	}	
+	}	    
 }
 </script>
+
+<style lang="scss">
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: 'Work Sans', sans-serif;
+}
+
+a {
+    text-decoration: none;
+}
+
+</style>
--- resources/js/guest/components/Header.vue
+++ resources/js/guest/components/Header.vue
@@ -1,14 +1,24 @@
 <template>
   <header>
-      <nav>
-          <ul>
-              <li><a href="/login">Login</a></li>
-              <li><a href="/register">Register</a></li>
-              <li></li>
-              <li></li>
-              <li></li>
-          </ul>
-      </nav>
+      <div class="container">
+        <nav>
+            <div class="nav-left">
+                <div class="logo">
+                    <img src="../../../images/logo.png" alt="logo Deliveboo">
+                </div>
+            </div>
+            <div class="nav-right">
+                <ul>
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="/register">Register</a></li>
+                    <li>
+                        <a href="#" class="cart"><span>0,00$</span> <img src="../../../images/cart_icon_white.png" alt="cart icon"></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+      </div>
+      
   </header>
 </template>
 
@@ -18,6 +28,51 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+@import '../../../sass/guest/front.scss';
+
+header {
+    height: 100px;
+    @include align-center;
+    background-color: aquamarine;
+    .container {
+        @include container-90;
+        nav {
+            @include align-center-between;
+            .nav-left {
+                .logo {
+                    width: 150px;
+                    img {
+                        width: 100%;
+                    }
+                }
+            }
+            .nav-right {
+                ul {
+                    @include inline-block-list;
+                    li {
+                        padding: 0 10px;
+                        a {
+                            text-transform: uppercase;
+                            color: $lightColor;
+                            font-weight: 500;
+                            font-size: 20px;
+                        }
+                        a.cart {
+                            @include btn($btnColor: $lightColor, $btnBG: transparent);
+                            font-size: 16px;
+                            padding: 10px;
+                            img {
+                                width: 30px;
+                                margin-left: 8px;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 </style>
