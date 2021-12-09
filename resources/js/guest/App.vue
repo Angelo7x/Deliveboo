50d4b12491d544dab605a2d7300874e88094f3d8
mariklim <maria.klimenko889@gmail.com>
2021-12-09 14:55:33 +0100

    add component CarouselCuisines

--- resources/js/guest/App.vue
+++ resources/js/guest/App.vue
@@ -3,6 +3,7 @@
         <Header/>
          <Hero/>
         <AppDeliveboo/>
+        <CarouselCuisines/>
         <main>
             <router-view></router-view>
         </main>
@@ -15,6 +16,7 @@ import Header from './components/Header.vue';
 import Hero  from './components/Hero.vue';
 import Footer  from './components/Footer.vue';
 import AppDeliveboo  from './components/AppDeliveboo.vue';
+import CarouselCuisines  from './components/CarouselCuisines.vue';
 
 export default {
 	name: 'App',
@@ -23,6 +25,7 @@ export default {
         Hero,
         Footer,
         AppDeliveboo,
+        CarouselCuisines,
 	}	    
 }
 </script>
