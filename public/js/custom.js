9fb57e2c6d86060bac83597e5b02aaf5303ea307
catanielena <catani.elena1@gmail.com>
2021-12-13 19:34:06 +0100

    Add: validation_back-end

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,6 +3,19 @@
 // Funzioni
 //
 //
+// {-- mostra l'immagine caricata --}
+const uploadedImage = function(idInput, idImage) {
+    document.getElementById(idInput).addEventListener('change', function () {
+        var src = URL.createObjectURL(this.files[0])
+        document.getElementById(idImage).src = src
+    })   
+}
+
+// call della funzione uploadedImage
+uploadedImage('image', 'foodEditPreview');
+uploadedImage('image', 'foodCreatePreview');
+uploadedImage('image', 'businessLogo');
+uploadedImage('image1', 'businessCover');
 // {-- mostra l'errore associato all'input --}
 function setErrorFor(input, message) {
 	var formControl = input.parentElement; //.form-control
@@ -38,7 +51,7 @@ function fileExists(file) {
 //
 //
 const allowedExtension = ["jpg", "jpeg", "png", "svg"];
-const maxfilesize = 1024 * 1024;
+const maxfilesize = 2000 * 2000;
 //
 //
 // Modale delete
