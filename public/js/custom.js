b90292b6d5c03986fc8eda2b82cfcf2080cfe87c
catanielena <catani.elena1@gmail.com>
2021-12-14 11:53:44 +0100

    Refact: custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,19 +3,6 @@
 // Funzioni
 //
 //
-// {-- mostra l'immagine caricata --}
-const uploadedImage = function(idInput, idImage) {
-    document.getElementById(idInput).addEventListener('change', function () {
-        var src = URL.createObjectURL(this.files[0])
-        document.getElementById(idImage).src = src
-    })   
-}
-
-// call della funzione uploadedImage
-uploadedImage('image', 'foodEditPreview');
-uploadedImage('image', 'foodCreatePreview');
-uploadedImage('image', 'businessLogo');
-uploadedImage('image1', 'businessCover');
 // {-- mostra l'errore associato all'input --}
 function setErrorFor(input, message) {
 	var formControl = input.parentElement; //.form-control
@@ -62,7 +49,9 @@ const inputDeleteID = document.getElementById("delete-id")
 deleteButtons.forEach(
 	(elm) => {
 		elm.addEventListener("click", function () {
+			alert('delete')
 			inputDeleteID.value = this.getAttribute("data-id");
+
 		});
 	}
 );
