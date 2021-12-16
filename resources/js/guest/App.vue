b2ab855b9a84547975b871cd93827f10c7644773
catanielena <catani.elena1@gmail.com>
2021-12-16 15:15:38 +0100

    Add: modal_change_restaurant

--- resources/js/guest/App.vue
+++ resources/js/guest/App.vue
@@ -2,8 +2,12 @@
   <div>
     <Header />
     <main>
-         <!-- @food="getFood" -->
-      <router-view :cart="cart" @food="getFood" @user_id="getUserId"></router-view>
+      <router-view
+        :cart="cart"
+        @food="getFood"
+        :isModalVisible="isModalVisible"
+        @clearCart="clearCart"
+      ></router-view>
     </main>
     <Footer />
   </div>
@@ -22,14 +26,15 @@ export default {
   data() {
     return {
       cart: {
-          'id': null, 
-          'items': []
-          },
+        id: null,
+        items: [],
+      },
       food: null,
       cartAction: 0,
       foodlist: [],
       action: null,
-      id: null
+      lastId: null,
+      isModalVisible: null
     };
   },
   methods: {
@@ -37,41 +42,49 @@ export default {
       this.cart.items.push({
         food: e,
         quantity: 1,
-      })
+      });
+    },
+    clearCart() {
+      localStorage.clear();
+      this.cart.items = [];
+      this.cart.id = null;
+      this.cartAction++;
     },
     getFood(e) {
       this.food = e.item;
       this.action = e.action;
+      // condizione cambio ristorante
+      if (this.cart.id !== null && this.cart.id !== e.item.user_id) {
+        this.isModalVisible = true;
+      } else {
+        this.cart.id = e.item.user_id;
+      }
       this.cartAction++;
     },
-    getUserId(e) {
-        this.id = e;
-    }
   },
   mounted() {
-       if(localStorage.getItem('cart')) {
-        this.cart.items = JSON.parse(localStorage.getItem('cart'));
-        // this.cart.id = this.id;
-        localStorage.clear();
+    if (localStorage.getItem("cart")) {
+      this.cart = JSON.parse(localStorage.getItem("cart"));
+      // localStorage.clear();
     }
   },
-watch: {
+  watch: {
     cartAction: function () {
       let inCart = false;
-      let cartItems = this.cart['items'];
+      let cartItems = this.cart["items"];
       let newFood = this.food;
       //aggiunta
-      if (this.action == 'add') {
-        if(cartItems.length <= 0) {
-          this.addToCart(newFood)
+      if (this.action == "add") {
+        if (cartItems.length <= 0) {
+          this.addToCart(newFood);
         } else {
-          cartItems.forEach((e,index) => {
-            if(newFood.id == e.food.id) {
+          cartItems.forEach((e, index) => {
+            if (newFood.id == e.food.id) {
               e.quantity++;
               inCart = true;
             }
           });
-          if(!inCart) {
+          if (!inCart) {
             this.addToCart(newFood);
           }
           inCart = false;
@@ -79,22 +92,23 @@ watch: {
         //rimozione
       } else {
         if (cartItems.length == 1 && cartItems[0].quantity == 1) {
-          this.cart['items'] = []
+          this.cart["items"] = [];
         } else {
-          cartItems.forEach((e,index) => {
-            if(newFood.id == e.food.id) {
+          cartItems.forEach((e, index) => {
+            if (newFood.id == e.food.id) {
               if (e.quantity > 1) {
                 e.quantity--;
               } else {
                 cartItems.splice(index, 1);
               }
             }
-          })
+          });
         }
       }
-      localStorage.setItem('cart', JSON.stringify(this.cart.items));
-    }
-  }
+      // salvataggio carrello
+      localStorage.setItem("cart", JSON.stringify(this.cart));
+    },
+  },
 };
 </script>
 
---
+++ resources/js/guest/components/ModalChangeCart.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <header class="modal-header">
+        <button type="button" class="btn-close" @click="close">x</button>
+      </header>
+
+      <section class="modal-body">
+        <slot name="body">
+          Sei sicuro di voler eliminare il carrello corrente e <br />
+          ordinare da un altro ristorante?
+        </slot>
+      </section>
+
+      <footer class="modal-footer">
+        <button type="button" class="btn-green" @click="clear">Procedi</button>
+        <button type="button" class="btn-green" @click="close">Annulla</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalChangeCart",
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+    clear() {
+      this.$emit("clearCart");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-header,
+.modal-footer {
+  padding: 15px;
+  display: flex;
+}
+
+.modal-header {
+  position: relative;
+  color: #4aae9b;
+  justify-content: space-between;
+}
+
+.modal-body {
+  position: relative;
+  padding: 20px 10px;
+}
+
+.btn-close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  font-size: 20px;
+  padding: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #4aae9b;
+  background: transparent;
+}
+
+.btn-green {
+  color: white;
+  background: #4aae9b;
+  border: 1px solid #4aae9b;
+  border-radius: 2px;
+}
+</style>
